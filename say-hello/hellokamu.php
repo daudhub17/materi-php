f8af1754c7a09705276b7b403cab8a4a479d58db
daud <assidiqidaud17@gmail.com>
2026-06-14 19:58:45 +0700

    chore: initial commit dengan file hellokamu.php

---
+++ say-hello/hellokamu.php
@@ -0,0 +1,9 @@
+<?php
+#Hello Kamu!
+
+echo "halo gessss";
+
+//ini adalah contoh dari bagaimana mencetak kalimat atau statement pada php.
+//echo disini kek bilang "php: haloo gesss"
+//echo itu ibarat php bicara kalimat yang kamu tulis.
+//loh knp bicara bukan tampilkan? hmmm nanti kita cari tahu :v
