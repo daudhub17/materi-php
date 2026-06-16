14192cb61ec537c12ad161941ffcd8ec264316d5
daud <assidiqidaud17@gmail.com>
2026-06-16 09:24:33 +0700

    feature: Tipe Data Number pada PHP

---
+++ TipeData/TipeDataNumber.php
@@ -0,0 +1,26 @@
+<?php
+
+#Tipe Data Number
+
+/* pada dasarnya tipe data number ada 2 : Float dan Integer */
+
+//data Integer
+
+echo 1 . PHP_EOL; // ini adalah contoh tipe data integer bilangan decimal
+var_dump(1);
+
+//basis data integer : decimal, octal, hexadecimal, binary.
+
+var_dump(0b11111); // basis binary
+
+
+//data Float
+
+echo 3.14 . PHP_EOL;
+var_dump(3.14);
+
+//data diatas merupakan jenis data floating point
+//pada floating point bisa juga digunakan dengan notasi eksponen
+
+var_dump(1.2e3);
+echo "\n";
